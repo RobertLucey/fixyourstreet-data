e87090e5103623d93e991d1de1dcde133ca5c0d2
Robert Lucey <robert@glimpse.ie>
2021-12-27 20:34:59 +0000

    Improve clean_description
    Also add a has_greeting and has_thanks since I want to see if it has an
    effect on being resolved

--- src/fixyourstreet_data/models/incident.py
+++ src/fixyourstreet_data/models/incident.py
@@ -1,4 +1,5 @@
 import datetime
+import string
 import re
 
 from fixyourstreet_data.models.location import Location
@@ -37,31 +38,62 @@ class Incident(GenericObject):
         return datetime.datetime.strptime(self.date, '%Y-%m-%d %H:%M:%S')
 
     @property
+    def has_greeting(self):
+        clean_description = self.clean_description.lower()
+        clean_description = clean_description.translate(
+            str.maketrans('', '', string.punctuation)
+        )
+        return set(
+            ['hi', 'hello', 'greetings', 'hey', 'dear']
+        ).intersection(set(clean_description.split())) != set()
+
+    @property
+    def has_thanks(self):
+        clean_description = self.clean_description.lower()
+        return any([
+            'thank you' in clean_description,
+            'thanks' in clean_description,
+            'cheers' in clean_description,
+            'regards' in clean_description,
+            'sincerely' in clean_description,
+        ]) or 'ta' == clean_description.split()[-1]
+
+    @property
     def clean_description(self):
-        string = self.description.replace('-- posted via the fixyourstreet.ie public api', '')
-        string = string.replace('\r\n----\r\n\r\n\r\n -- posted via the fixyourstreet.ie public api', '')
-        string = string.replace('\r\n\r\n. Submitted via EPA/NIECE Smartphone App.', '')
-        string = string.replace('Submitted via EPA/NIECE Smartphone App.', '')
-        string = string.replace('----\r\nThis report was originally submitted at FixMyStreet.ie. You can find it at this alternate address:', '')
-        string = string.replace('-- posted via fixyourstreet.ie mobile web', '')
-        string = string.replace('-- posted via fixyourstreet.ie', '')
-        string = string.replace('-- received via Twitter (Contact us at @fixyourstreet or using the hashtag #fysie). Catch up with us on http://twitter.com/fixyourstreet', '')
-        string = string.replace('#Waste/IllegalDumping', '')
-        string = string.replace('#Air/Odour', '')
-        string = string.replace('#Water', '')
-        string = string.replace('#DrinkingWater', '')
-        string = string.replace('#PublicLighting', '')
-        string = string.replace('#Noise', '')
-        string = string.replace('----', '')
-        string = string.replace('\r\n.', '.')
-        string = string.replace('\r\n\r\n', '\r\n')
-        string = string.replace('Ã¢Â€Â™', '\'')
-        string = string.replace('Â’', '\'')
+        clean_description = self.description.replace('-- posted via the fixyourstreet.ie public api', '')
+        clean_description = clean_description.replace('\r\n----\r\n\r\n\r\n -- posted via the fixyourstreet.ie public api', '')
+        clean_description = clean_description.replace('\r\n\r\n. Submitted via EPA/NIECE Smartphone App.', '')
+        clean_description = clean_description.replace('Submitted via EPA/NIECE Smartphone App.', '')
+        clean_description = clean_description.replace('----\r\nThis report was originally submitted at FixMyStreet.ie. You can find it at this alternate address:', '')
+        clean_description = clean_description.replace('-- posted via fixyourstreet.ie mobile web', '')
+        clean_description = clean_description.replace('-- posted via fixyourstreet.ie', '')
+        clean_description = clean_description.replace('posted via fixyourstreet.ie', '')
+        clean_description = clean_description.replace('-- received via Twitter (Contact us at @fixyourstreet or using the hashtag #fysie). Catch up with us on http://twitter.com/fixyourstreet', '')
+        clean_description = clean_description.replace('#Waste/IllegalDumping', '')
+        clean_description = clean_description.replace('#Air/Odour', '')
+        clean_description = clean_description.replace('#Water', '')
+        clean_description = clean_description.replace('#DrinkingWater', '')
+        clean_description = clean_description.replace('#PublicLighting', '')
+        clean_description = clean_description.replace('#Noise', '')
+        clean_description = clean_description.replace('----', '')
+        clean_description = clean_description.replace('\r\n.', '.')
+        clean_description = clean_description.replace('Ã¢Â€Â™', '\'')
+        clean_description = clean_description.replace('Â’', '\'')
+        clean_description = clean_description.replace('\'Â€Â™', '\'')
+        clean_description = clean_description.replace('Â”', '”')
 
-        string = re.sub(r'https:\/\/fixmystreet\.ie\/report\/\d+', '', string)
-        string = re.sub(r' +', ' ', string)
+        clean_description = clean_description.replace('!.', '!')
+        clean_description = clean_description.replace('?.', '?')
+        clean_description = clean_description.replace(' .', '.')
 
-        return string.strip()
+        clean_description = re.sub(r'https:\/\/fixmystreet\.ie\/report\/\d+', '', clean_description)
+        clean_description = re.sub(r' +', ' ', clean_description)
+        clean_description = re.sub('(\r\n)+', '\r\n', clean_description)
+
+        lines = clean_description.split('\r\n')
+        clean_description = '\r\n'.join([l.strip() for l in lines])
+
+        return clean_description.strip()
 
     def serialize(self, minimal=True):
         data = {
